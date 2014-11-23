3f9a73d53a8501fe2e5e6a0f34b685991ecb701c
Felix Geller <fgeller@gmail.com>
2014-11-23 22:34:30 +1300

    Initial commit.

---
+++ minimal.el
@@ -0,0 +1,52 @@
+(deftheme minimal "Minimalistic theme based on base16 colors.")
+
+(defvar base16-colors-alist
+  '(("black" . "#181818")
+    ("gray1" . "#282828")
+    ("gray2" . "#383838")
+    ("gray3" . "#585858")
+    ("gray4" . "#b8b8b8")
+    ("gray5" . "#d8d8d8")
+    ("gray6" . "#e8e8e8")
+    ("white" . "#f8f8f8")
+    ("red" . "#ab4642")
+    ("orange" . "#dc9656")
+    ("yellow" . "#f7ca88")
+    ("green" . "#a1b56c")
+    ("turquoise" . "#86c1b9")
+    ("blue" . "#7cafc2")
+    ("purple" . "#ba8baf")
+    ("brown" . "#a16046")))
+
+(defmacro base16/with-color-variables (&rest body)
+  (declare (indent 0))
+  `(let ((class '((class color) (min-colors 89)))
+         ,@(mapcar (lambda (cons)
+                     (list (intern (car cons)) (cdr cons)))
+                   base16-colors-alist))
+     ,@body))
+
+;;; Theme Faces
+(base16/with-color-variables
+  (custom-theme-set-faces
+   'minimal
+   `(default ((t (:foreground ,black :background ,white))))
+   `(fringe ((t (:foreground ,gray3 :background ,gray6))))
+   `(hi-yellow ((t (:foreground ,red :background ,white))))
+   `(isearch ((t (:foreground nil :background ,blue))))  
+   `(isearch-fail ((t (:foreground nil :background ,orange))))
+   `(lazy-highlight ((t (:foreground nil :background ,blue))))
+   `(linum  ((t (:foreground ,gray3 :background ,gray6))))
+   `(minibuffer-prompt ((t (:foreground ,red :background nil))))
+   `(mode-line ((t (:foreground ,gray3 :background ,gray6))))
+   `(mode-line-buffer-id ((t (:weight normal))))
+   `(mode-line-inactive ((t (:foreground ,gray3 :background ,gray6))))
+   `(region ((t (:foreground nil :background ,yellow))))
+   `(trailing-whitespace ((t (:foreground nil :background ,red))))
+
+   `(helm-candidate-number ((t (:foreground ,gray3 :background ,gray6))))
+   `(helm-header ((t (:foreground ,gray3 :background ,gray6 :box nil))))
+   `(helm-selection ((t (:foreground nil :background ,yellow :underline nil))))
+   `(helm-source-header ((t (:foreground ,gray3 :background ,blue :underline nil))))
+   ))
+
