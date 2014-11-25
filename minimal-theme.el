89a00b7b4d6bc07e0e3b5c0de348ccd05e128bb3
Felix Geller <fgeller@gmail.com>
2014-11-25 19:56:08 +1300

    Drops superfluous configuration.

--- minimal-theme.el
+++ minimal-theme.el
@@ -99,10 +99,4 @@
    `(magit-tag ((t (:foreground ,gray4 :background nil))))
    ))
 
-(and load-file-name
-     (boundp 'custom-theme-load-path)
-     (add-to-list 'custom-theme-load-path
-                  (file-name-as-directory
-                   (file-name-directory load-file-name))))
-
 (provide-theme 'minimal)
