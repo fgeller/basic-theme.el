7bbadabf1b2e724ae3f0457115ac5a3dce730694
Felix Geller <fgeller@gmail.com>
2014-12-07 15:01:01 +1300

    Automatically add theme to load path.

--- basic-theme.el
+++ basic-theme.el
@@ -199,6 +199,13 @@
        (350 . ,gray3)))
    ))
 
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide-theme 'basic)
 
 ;; Local Variables:
